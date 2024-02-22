323867fd9856c2a0fad2a7f37eb16190eeb0e210
dayyear <15589559@qq.com>
2024-02-22 21:58:03 +0800

    Update aliyun.py

--- aliyun.py
+++ aliyun.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
-refresh_token = '82df0657541a4562b1ff299030417b7d'
+refresh_token = '967f4df669704e3b89f360bc049c3eba'
 
 # 登录
 response = requests.post(url=f'https://auth.aliyundrive.com/v2/account/token', json={'grant_type': 'refresh_token', 'refresh_token': refresh_token}, headers=headers)
